1371d1f3dd68bde700eba12681242a42a46e1d9c
Elias Chacin <elichaci@publicisgroupe.net>
2019-02-05 12:09:29 -0500

    oop

--- App.js
+++ App.js
@@ -1,98 +1,70 @@
 let grid = [];
-let rows = 10;
-let cols = 10;
-let initial_prob = 1;
+let prev_grid = [];
+
+const rows = 10;
+const cols = 10;
+const initial_prob = 0;
 let live_cells = 0;
 
 //creation of cells in grid and display scoreboard
 $(document).ready(function () {
-    let div_str = "<div class=\"cell\"/>";
+    let div_str = '<div class=\"cell\"/>';
 
     for (let i = 0; i < rows*cols; i++) {
-        let rand = Math.random() >= initial_prob ? true : false;
+        let rand = Math.random() <= initial_prob ? true : false;
         let newCell = new Cell(i, rand);
+        grid.push(newCell);
 
-        $("#wrapper").append($(div_str).clone()
+        $('#wrapper').append($(div_str).clone()
             .attr('id', newCell.id)
             .css('background-color', '#EE7600')
             .css('opacity', newCell.status ? 1 : 0));
-        grid.push(newCell);
         if(newCell.status) live_cells++;
-        $('#score-board').html("Live Cells: " + '<span style=color:#EE7600>' + live_cells + '</span>');
+        showScoreboard();
     }
+
     $('.cell').click(function(e) {
-        toggleLife(grid[e.target.id]);
-    } );
-
-});
-
-//click handler callback
-function toggleLife(cell){
-    if(cell.status){
-        cell.death();
-    }else{
+        cell = grid[e.target.id];
         console.log(cell.id);
-        cell.birth();
-    }
-    $("#score-board").html("Live Cells: " + '<span style=color:#EE7600>' + live_cells + '</span>');
-
-}
+        if(cell.status){
+            cell.death();
+            live_cells--;
+        }else{
+            cell.birth();
+            live_cells++;
+        }
+        showScoreboard();
+    } );
+});
 
 //spawn generation
 function generation(){
     live_cells = 0;
 
-    grid.forEach(function(cell){
+
+    //TO DO: make DEEP copy of grid array!!! shallow copy won't work
+    prev_grid = grid;
+
+    prev_grid.forEach(function(cell){
+
+        let num = cell.getNumLiveNeighbors();
         if(cell.status){
-            if(cell.getNumLiveNeighbors() < 2){
+            if(num < 2 || num > 3){
                 cell.death();
-            }else if(cell.getNumLiveNeighbors() > 3){
-                cell.death();
+                live_cells--;
             }
         }else{
-            if(cell.getNumLiveNeighbors() == 3){
+            if(num == 3){
                 cell.birth();
+                live_cells++;
             }
         }
     });
-    $("#score-board").html("Live Cells: " + '<span style=color:#EE7600>' + live_cells + '</span>');
+    showScoreboard();
 }
 
-//Cell class
-class Cell{
-    constructor(id, status){
-        this.id = id;
-        this.status = status;
-    }
+//show SCOREBOARD
+function showScoreboard(){
+    $('#score-board').html('Live Cells:'  + '<span style=color:#EE7600>' + live_cells + '</span>');
 
-    getNeighbors(){
-        let neighbors =  [
-            grid[this.id - cols - 1], grid[this.id - cols], grid[this.id - cols + 1], 
-            grid[this.id - 1], grid[this.id + 1],
-            grid[this.id + cols -1], grid[this.id + cols], grid[this.id + cols + 1]
-        ];
-
-        return neighbors;
-    }
-
-    getNumLiveNeighbors(){
-        let arr = this.getNeighbors().filter(function(cell){
-            if(cell){
-                return cell.status;
-            }
-        });
-        return arr.length;
-    }
-
-    death(){
-        this.status = false;
-        $('#' + this.id).animate({ opacity: 0 });
-        live_cells--;
-    }
-
-    birth(){
-        this.status = true;
-        $('#' + this.id).animate({ opacity: 1 });
-        live_cells++;
-    }
 }
